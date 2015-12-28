822198117fdabed5e6a3f781177e85aa21e85a58
Aaron Preston <aaronpreston115@gmail.com>
2015-12-28 04:19:07 -0500

    Add current game files.

---
+++ scripts/js/positioncheck.js
@@ -0,0 +1,99 @@
+posCheck = function() {
+  if(player.x >= 60 && player.y >= 30 && player.x <= 225 && player.y <= 177) {
+
+
+  } else if((player.x >= 225 && player.y >= 27 && player.x <= 267 && player.y <= 30) ||
+            (player.x >= 234 && player.y >= 24 && player.x <= 264 && player.y <= 30) ||
+            (player.x >= 246 && player.y >= 21 && player.x <= 264 && player.y <= 30)){
+
+
+  } else if((player.x >= 105 && player.y >= 27 && player.x <= 222 && player.y <= 30) ||
+            (player.x >= 123 && player.y >= 24 && player.x <= 186 && player.y <= 30) ||
+            (player.x >= 126 && player.y >= 21 && player.x <= 180 && player.y <= 30) ||
+            (player.x >= 132 && player.y >= 18 && player.x <= 180 && player.y <= 30) ||
+            (player.x >= 156 && player.y >= 15 && player.x <= 180 && player.y <= 30) ||
+            (player.x === 183 && player.y === 15) ||
+            (player.x >= 204 && player.y >= 24 && player.x <= 216 && player.y <= 30)) {
+
+
+  } else if((player.x >= 60 && player.y >= 27 && player.x <= 87 && player.y <= 30) ||
+            (player.x >= 63 && player.y >= 24 && player.x <= 87 && player.y <= 30) ||
+            (player.x >= 69 && player.y >= 21 && player.x <= 87 && player.y <= 30) ||
+            (player.x >= 78 && player.y >= 18 && player.x <= 87 && player.y <= 30)) {
+
+
+  } else if((player.x >= 228 && player.y >= 33 && player.x <= 270 && player.y <= 39) ||
+            (player.x >= 228 && player.y >= 42 && player.x <= 267 && player.y <= 45) ||
+            (player.x >= 228 && player.y >= 48 && player.x <= 264 && player.y <= 51) ||
+            (player.x >= 228 && player.y >= 54 && player.x <= 261 && player.y <= 57) ||
+            (player.x >= 228 && player.y >= 60 && player.x <= 255 && player.y <= 60) ||
+            (player.x >= 228 && player.y >= 63 && player.x <= 243 && player.y <= 63) ||
+            (player.x === 228 && player.y === 66) ||
+            (player.x === 231 && player.y === 66)) {
+
+
+  } else if((player.x === 228 && player.y === 81) ||
+            (player.x >= 228 && player.y >= 84 && player.x <= 231 && player.y <= 177) ||
+            (player.x >= 234 && player.y >= 90 && player.x <= 234 && player.y <= 180) ||
+            (player.x >= 237 && player.y >= 96 && player.x <= 237 && player.y <= 183) ||
+            (player.x >= 240 && player.y >= 99 && player.x <= 240 && player.y <= 186) ||
+            (player.x >= 243 && player.y >= 102 && player.x <= 243 && player.y <= 216) ||
+            (player.x === 240 && player.y >= 201 && player.y <= 216) ||
+            (player.x === 237 && player.y >= 204 && player.y <= 216) ||
+            (player.x === 234 && player.y >= 210 && player.y <= 213) ||
+            (player.x === 246 && player.y >= 108 && player.y <= 213) ||
+            (player.x === 249 && player.y >= 111 && player.y <= 213) ||
+            (player.x === 252 && player.y >= 117 && player.y <= 213) ||
+            (player.x === 255 && player.y >= 126 && player.y <= 210) ||
+            (player.x === 258 && player.y >= 129 && player.y <= 210) ||
+            (player.x === 261 && player.y >= 132 && player.y <= 210) ||
+            (player.x === 264 && player.y >= 138 && player.y <= 207) ||
+            (player.x === 267 && player.y >= 147 && player.y <= 207) ||
+            (player.x === 270 && player.y >= 156 && player.y <= 162) ||
+            (player.x === 270 && player.y >= 171 && player.y <= 207) ||
+            (player.x === 273 && player.y >= 174 && player.y <= 207) ||
+            (player.x === 276 && player.y >= 180 && player.y <= 201) ||
+            (player.x === 279 && player.y === 186)) {
+
+
+  } else if((player.x >= 33 && player.y === 180 && player.x <= 213) ||
+            (player.x >= 36 && player.y === 183 && player.x <= 210) ||
+            (player.x >= 36 && player.y === 186 && player.x <= 42) ||
+            (player.x >= 54 && player.y === 186 && player.x <= 201) ||
+            (player.x >= 39 && player.y === 189 && player.x <= 42) ||
+            (player.x === 39 && player.y === 192) ||
+            (player.x >= 54 && player.y === 189 && player.x <= 195) ||
+            (player.x >= 54 && player.y === 192 && player.x <= 186) ||
+            (player.x >= 60 && player.y === 195 && player.x <= 165) ||
+            (player.x >= 66 && player.y === 198 && player.x <= 144) ||
+            (player.x >= 60 && player.y === 201 && player.x <= 117) ||
+            (player.x >= 57 && player.y === 204 && player.x <= 69)) {
+
+
+  } else if((player.x === 57 && player.y >= 33 && player.y <= 177) ||
+            (player.x === 54 && player.y >= 42 && player.y <= 177) ||
+            (player.x === 51 && player.y >= 45 && player.y <= 177) ||
+            (player.x === 48 && player.y >= 48 && player.y <= 177) ||
+            (player.x === 45 && player.y >= 51 && player.y <= 177) ||
+            (player.x === 42 && player.y >= 51 && player.y <= 177) ||
+            (player.x === 39 && player.y >= 51 && player.y <= 138) ||
+            (player.x === 36 && player.y >= 60 && player.y <= 129) ||
+            (player.x === 33 && player.y >= 75 && player.y <= 114) ||
+            (player.x === 30 && player.y >= 81 && player.y <= 117) ||
+            (player.x === 27 && player.y >= 93 && player.y <= 120) ||
+            (player.x === 24 && player.y >= 96 && player.y <= 126) ||
+            (player.x === 21 && player.y >= 99 && player.y <= 126) ||
+            (player.x === 18 && player.y >= 102 && player.y <= 120) ||
+            (player.x === 39 && player.y >= 150 && player.y <= 177) ||
+            (player.x === 36 && player.y >= 156 && player.y <= 177) ||
+            (player.x === 33 && player.y >= 159 && player.y <= 177) ||
+            (player.x === 30 && player.y === 168)){
+
+
+  } else {
+    player.x = player.last_x;
+    player.y = player.last_y;
+  }
+
+  document.getElementById('coordinates').innerHTML = 'X: ' + player.x + ' | Y: ' + player.y;
+}
